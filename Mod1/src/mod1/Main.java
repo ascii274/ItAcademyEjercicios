f9b3ba84844260323642eb5c9883f5dbb9ca63e7
ascii274 <joel@darkstar.Home>
2021-06-21 11:25:34 +0200

    Exercici Nivell 1 fase 1

--- Mod1/src/mod1/Main.java
+++ Mod1/src/mod1/Main.java
@@ -1,9 +1,27 @@
 
 package mod1;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        //nivell 1 fase 1     
+        mostraDadesMessi(scanner);  
+    }
+    
+       static void mostraDadesMessi(Scanner scanner){     
+        // fase 1
+        String  nom = "Lionel",
+                cognom1 = "Cuccittini",
+                cognom2 = "Messi";
+        int dia =24,
+            mes = 6, 
+            any = 1987;    
+        System.out.println("\n***** Dades Messi *****");
+        System.out.println( cognom1 + " " + cognom2 + ", " + nom);
+        System.out.println(dia + "/" + mes + "/" + any);        
     }
     
 }
